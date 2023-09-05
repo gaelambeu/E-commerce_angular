aadbfc2fd6f9513743c57f63afdd175f28e3f37f
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-05 22:51:10 +0000

    change

--- ecommerce-app/src/app/app-routing.module.ts
+++ ecommerce-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductViewComponent } from './component/product-view/product-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ProductViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
