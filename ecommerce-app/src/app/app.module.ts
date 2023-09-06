e202bd4e260dda941dd4d8d5129ac2d1b88e1ae0
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-06 22:40:56 +0000

    making api

--- ecommerce-app/src/app/app.module.ts
+++ ecommerce-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductViewComponent } from './component/product-view/product-view.component';
@@ -12,7 +12,8 @@ import { ProductViewComponent } from './component/product-view/product-view.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
