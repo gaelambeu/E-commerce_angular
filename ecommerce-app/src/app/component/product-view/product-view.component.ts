f18f8833e04dd53250ffc5c70d509cb9e11e1c07
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-07 23:09:57 +0000

    api making

--- ecommerce-app/src/app/component/product-view/product-view.component.ts
+++ ecommerce-app/src/app/component/product-view/product-view.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/api.service';
+import { product } from './productmodal';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.css']
 })
-export class ProductViewComponent {
+export class ProductViewComponent implements OnInit {
+  data!:product[]
+  constructor(private api:ApiService){}
+  ngOnInit(): void {
+    
+  }
 
+  displayproducts(){
+    this.api.getproduct().subscribe(res=> {
+      this.data = res;
+    })
+  }
 }
